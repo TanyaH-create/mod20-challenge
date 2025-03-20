d4147c1f939234b014085f9a0e78b31032ef3b46
Tanya <dougtanyah@gmail.com>
2025-03-19 22:47:04 -0400

    fix: correct cleanDb.ts - was bad code

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,14 +1,37 @@
-import models from '../models/index.js';
-import db from '../config/connection.js';
+// import models from '../models/index.js';
+// import db from '../config/connection.js';
 
-export default async (modelName: "Question", collectionName: string) => {
+// export default async (modelName: "Question", collectionName: string) => {
+
+//   try {
+//     let modelExists = await models[modelName].db.db.listCollections({
+//       name: collectionName
+//     }).toArray()
+
+//     if (modelExists.length) {
+//       await db.dropCollection(collectionName);
+//     }
+//   } catch (err) {
+//     throw err;
+//   }
+// }
+import connection from '../config/connection.js';
+
+export default async (_modelName: string, collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    // Check if the collection exists using the connection directly
+    const db = connection.db;
+    
+    if (!db) {
+      throw new Error('Database connection not established');
+    }
+    
+    const modelExists = await db
+      .listCollections({ name: collectionName })
+      .toArray();
 
     if (modelExists.length) {
-      await db.dropCollection(collectionName);
+      await connection.dropCollection(collectionName);
     }
   } catch (err) {
     throw err;
